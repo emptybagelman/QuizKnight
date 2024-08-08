98bd57125700a119400e462e88457188ae61b3c5
emptybagelman <80276062+emptybagelman@users.noreply.github.com>
2024-08-08 08:11:32 +0100

    Update index.tsx

--- src/app/_components/GameWindow/Combat/index.tsx
+++ src/app/_components/GameWindow/Combat/index.tsx
@@ -34,9 +34,9 @@ export default function Combat(){
 
     const enemyArray: Enemy[] = [...Array.from({length:enemyAmount}).map((x, index) => {
 
-        const hp = randomInt((2 + Math.floor(loop*1.06)),1)
+        const hp = randomInt((2 + Math.floor(loop*1.06)),2)
         const dmg = randomInt((2 + Math.floor(loop*1.04)),1)
-        const armour = randomInt((1 + Math.floor(loop*1.02)),1)
+        const armour = randomInt((1 + Math.floor(loop*1.05)),1)
 
         const en = {
             id: index,
