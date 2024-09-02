14c87055ca74dd99618aa1211a63dffc6e7d0fe7
emptybagelman <80276062+emptybagelman@users.noreply.github.com>
2024-09-02 12:56:37 +0100

    Update index.tsx

--- src/app/_components/GameContext/index.tsx
+++ src/app/_components/GameContext/index.tsx
@@ -25,7 +25,7 @@ const defaultPlayerState: PlayerType = {
         id: 0,
         name: "Power Move",
         type: "damage",
-        charge: 100,
+        charge: 0,
     }],
     consumables: [{
         id: 0,
@@ -36,7 +36,7 @@ const defaultPlayerState: PlayerType = {
     {
         id: 1,
         name: "Mana Potion",
-        value: 1,
+        value: 0,
         description: "Charge your power move to the max!"
     },
     {
