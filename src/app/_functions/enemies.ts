2c3aba5d22d4e20849f2cd466a5345f991471398
emptybagelman <80276062+emptybagelman@users.noreply.github.com>
2024-09-09 13:12:21 +0100

    Update enemies.ts

--- src/app/_functions/enemies.ts
+++ src/app/_functions/enemies.ts
@@ -7,23 +7,23 @@ export function enemyStats(name: string, loop:number) {
 
     switch (name){
         case "Goblin":
-            hp = randomInt((3 + Math.floor(loop * 0.8)), 1)
-            dmg = randomInt((3 + Math.floor(loop * 0.7)), 1)
+            hp = randomInt((3 + Math.floor(loop * 0.4)), 1)
+            dmg = randomInt((3 + Math.floor(loop * 0.5)), 1)
             armour = randomInt((0 + Math.floor(loop * 0.2)), 1)
             break;
         case "Mushroom":
-            hp = randomInt((3 + Math.floor(loop * 1)), 2)
-            dmg = randomInt((1 + Math.floor(loop * 0.5)), 2)
+            hp = randomInt((3 + Math.floor(loop * 0.7)), 2)
+            dmg = randomInt((1 + Math.floor(loop * 0.35)), 2)
             armour = randomInt((1 + Math.floor(loop * 0.3)), 1)
             break;
         case "Skeleton":
-            hp = randomInt((2 + Math.floor(loop * 0.6)), 1)
-            dmg = randomInt((3 + Math.floor(loop * 0.8)), 1)
+            hp = randomInt((2 + Math.floor(loop * 0.55)), 1)
+            dmg = randomInt((3 + Math.floor(loop * 0.7)), 1)
             armour = randomInt((1 + Math.floor(loop * 0.2)), 1)
             break;
         case "Flying Eye":
-            hp = randomInt((3 + Math.floor(loop * 0.7)), 2)
-            dmg = randomInt((2 + Math.floor(loop * 0.7)), 1)
+            hp = randomInt((3 + Math.floor(loop * 0.6)), 2)
+            dmg = randomInt((2 + Math.floor(loop * 0.6)), 1)
             armour = 0
             break;
         case "Toad":
