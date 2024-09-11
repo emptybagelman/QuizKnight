ab09fcc25a28806ffa6c4d20c453f3e5294d19ac
emptybagelman <80276062+emptybagelman@users.noreply.github.com>
2024-09-11 23:15:11 +0100

    Update types.tsx

--- src/app/_types/types.tsx
+++ src/app/_types/types.tsx
@@ -46,7 +46,7 @@ export type StatType = {
     name: string
 }
 
-export type CharacterNames = "Player" | "Goblin" | "Mushroom" | "Skeleton" | "Flying Eye" | "Toad" | "Demon Slime" | string
+export type CharacterNames = "Player" | "Goblin" | "Mushroom" | "Skeleton" | "Flying Eye" | "Toad" | "Demon Slime"
 
 export type ConsumableNames = "Health Potion" | "Mana Potion" | "Agility Crystal" | "Firebomb"
 
